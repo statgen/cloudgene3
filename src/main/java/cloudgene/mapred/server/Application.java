48dcd05883252b985e04fcb449add005eb997dc9
Marc Fraile <marc.fraile.fabrega@gmail.com>
2026-01-13 14:05:02 -0500

    Bump version to v3.1.4-statgen.9

--- src/main/java/cloudgene/mapred/server/Application.java
+++ src/main/java/cloudgene/mapred/server/Application.java
@@ -29,7 +29,7 @@ import io.micronaut.context.annotation.Context;
 @Context
 public class Application {
 
-	public static final String VERSION = "3.1.4-statgen.8";
+	public static final String VERSION = "3.1.4-statgen.9";
 
 	private Database database;
 
