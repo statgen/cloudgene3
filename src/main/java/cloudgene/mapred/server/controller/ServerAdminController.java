52860703b49442fe7f48b3886d7bdaf05084a14e
Marc Fraile <marc.fraile.fabrega@gmail.com>
2026-02-16 10:35:22 -0500

    Bugfix: ensure back-end doesn't fail if /api/v2/admin/server/settings/update receives empty googleAnalytics (front-end sometimes sends empty string, sometimes sends null-ish value).

--- src/main/java/cloudgene/mapred/server/controller/ServerAdminController.java
+++ src/main/java/cloudgene/mapred/server/controller/ServerAdminController.java
@@ -93,17 +93,29 @@ public class ServerAdminController {
 	}
 
 	@Post("/settings/update")
-	public ServerResponse updateSettings(String name, String adminName, String adminMail, String serverUrl, String baseUrl,
-			String backgroundColor, String foregroundColor, String googleAnalytics, boolean mail, String mailSmtp,
-			String mailUser, String mailPassword, String mailPort, String mailName, String workspaceType,
+	public ServerResponse updateSettings(
+			String name,
+			String adminName,
+			String adminMail,
+			String serverUrl,
+			String baseUrl,
+			String backgroundColor,
+			String foregroundColor,
+			@Nullable String googleAnalytics,
+			boolean mail,
+			String mailSmtp,
+			String mailUser,
+			String mailPassword,
+			String mailPort,
+			String mailName,
+			String workspaceType,
 			String workspaceLocation) {
 
-		serverService.updateSettings(name, adminName, adminMail, serverUrl, baseUrl,  backgroundColor, foregroundColor,
+		serverService.updateSettings(name, adminName, adminMail, serverUrl, baseUrl, backgroundColor, foregroundColor,
 				googleAnalytics, String.valueOf(mail), mailSmtp, mailPort, mailUser, mailPassword, mailName,
 				workspaceType, workspaceLocation);
 
 		return ServerResponse.build(application.getSettings());
-
 	}
 
 	@Get("/nextflow/config")
@@ -132,7 +144,5 @@ public class ServerAdminController {
 				System.currentTimeMillis() - (1000L * 60L * 60L * 24L * days), System.currentTimeMillis());
 
 		return StatisticsResponse.build(stats);
-
 	}
-
 }
