53c8bd366faa86fe23900b580f595fddbe7dc15f
Marc Fraile <marc.fraile.fabrega@gmail.com>
2026-02-24 17:01:01 -0500

    Curb test duration of the most egregious slow test.

--- src/test/java/cloudgene/mapred/jobs/PriorityThreadPoolExecutorTest.java
+++ src/test/java/cloudgene/mapred/jobs/PriorityThreadPoolExecutorTest.java
@@ -3,7 +3,6 @@ package cloudgene.mapred.jobs;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -22,106 +21,97 @@ import genepi.io.FileUtil;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 
+// TODO(Marc): This seems to be a misnomer. This class seems to ignore
+//             PriorityThreadPoolExecutor and instead tests WorkflowEngine. We should
+//             rename the existing WorkflowEngineTest to whatever the hell it's actually
+//             testing, and rename this class to WorkflowEngineTest.
+
 @MicronautTest
 public class PriorityThreadPoolExecutorTest {
 
-	private static final int WAIT_FOR_CANCEL = 8000;
-
-	@Inject
-	TestApplication application;
+	private static final int WAIT_FOR_CANCEL = 1_100;
 
 	@Inject
 	WorkspaceFactory workspaceFactory;
 
 	@Test
 	public void testCancelRunningJob() throws Exception {
-
+		TestApplication application = new TestApplication();
 		WorkflowEngine engine = application.getWorkflowEngine();
 
-		while (engine.getAllJobsInLongTimeQueue().size() > 0) {
-			Thread.sleep(6000);
-		}
+		WdlApp wdlApp = WdlReader.loadAppFromFile("test-data/long-sleep.yaml");
 
-		WdlApp app = WdlReader.loadAppFromFile("test-data/long-sleep.yaml");
+		Map<String, String> inputs = Map.of("input", "input-file");
 
-		Map<String, String> inputs = new HashMap<String, String>();
-		inputs.put("input", "input-file");
 		List<AbstractJob> jobsBeforeSubmit = engine.getAllJobsInLongTimeQueue();
+		assertEquals(0, jobsBeforeSubmit.size());
 
-		AbstractJob job1 = createJobFromWdl(app, "job_running", inputs);
+		AbstractJob job1 = createJobFromWdl(application, wdlApp, "job_running", inputs);
 		engine.submit(job1);
+
 		while (job1.getState() == JobState.WAITING) {
-			Thread.sleep(1000);
+			Thread.sleep(100);
 		}
 
 		assertEquals(JobState.RUNNING, job1.getState());
 
 		List<AbstractJob> jobsAfterSubmit = engine.getAllJobsInLongTimeQueue();
-		assertEquals(jobsBeforeSubmit.size() + 1, jobsAfterSubmit.size());
+		assertEquals(1, jobsAfterSubmit.size());
 
 		engine.cancel(job1);
+
 		while (job1.getState() == JobState.RUNNING) {
-			Thread.sleep(1000);
+			Thread.sleep(100);
 		}
+
 		assertEquals(JobState.CANCELED, job1.getState());
-		Thread.sleep(10000);
 
-		List<AbstractJob> jobsAfterCancel = engine.getAllJobsInLongTimeQueue();
-
-		while (engine.getAllJobsInLongTimeQueue().size() > 0) {
-			Thread.sleep(6000);
-		}
-
+		application.stop(null);
 	}
 
 	@Test
 	public void testCancelWaitingJob() throws Exception {
-
+		TestApplication application = new TestApplication();
 		WorkflowEngine engine = application.getWorkflowEngine();
 
-		while (engine.getAllJobsInLongTimeQueue().size() > 0) {
-			Thread.sleep(6000);
-		}
+		WdlApp wdlApp = WdlReader.loadAppFromFile("test-data/long-sleep.yaml");
 
-		WdlApp app = WdlReader.loadAppFromFile("test-data/long-sleep.yaml");
+		Map<String, String> inputs = Map.of("input", "input-file");
 
-		Map<String, String> inputs = new HashMap<String, String>();
-		inputs.put("input", "input-file");
 		List<AbstractJob> jobsBeforeSubmit = engine.getAllJobsInLongTimeQueue();
+		assertEquals(0, jobsBeforeSubmit.size());
 
-		AbstractJob job1 = createJobFromWdl(app, "job_running_a", inputs);
+		AbstractJob job1 = createJobFromWdl(application, wdlApp, "job_running_a", inputs);
 		engine.submit(job1);
+
 		while (job1.getState() == JobState.WAITING) {
-			Thread.sleep(1000);
+			Thread.sleep(100);
 		}
 
-		AbstractJob job2 = createJobFromWdl(app, "job_waiting_b", inputs);
+		AbstractJob job2 = createJobFromWdl(application, wdlApp, "job_waiting_b", inputs);
 		engine.submit(job2);
 
-		Thread.sleep(5000);
+		Thread.sleep(1_000);
 
 		assertEquals(JobState.RUNNING, job1.getState());
 		assertEquals(JobState.WAITING, job2.getState());
 
 		List<AbstractJob> jobsAfterSubmit = engine.getAllJobsInLongTimeQueue();
-		assertEquals(jobsBeforeSubmit.size() + 2, jobsAfterSubmit.size());
+		assertEquals(2, jobsAfterSubmit.size());
 
 		engine.cancel(job2);
 		while (job2.getState() == JobState.RUNNING) {
-			Thread.sleep(1000);
+			Thread.sleep(100);
 		}
 
 		assertEquals(JobState.CANCELED, job2.getState());
 
 		List<AbstractJob> jobsAfterCancel = engine.getAllJobsInLongTimeQueue();
-		assertEquals(jobsAfterSubmit.size() - 1, jobsAfterCancel.size());
+		assertEquals(1, jobsAfterCancel.size());
 
 		// clear queue
 		engine.cancel(job1);
-
-		while (engine.getAllJobsInLongTimeQueue().size() > 0) {
-			Thread.sleep(6000);
-		}
+		application.stop(null);
 	}
 
 	/**
@@ -132,37 +122,32 @@ public class PriorityThreadPoolExecutorTest {
 	 */
 	@Test
 	public void testMultipleJobs() throws Exception {
-
+		TestApplication application = new TestApplication();
 		WorkflowEngine engine = application.getWorkflowEngine();
 
-		while (engine.getAllJobsInLongTimeQueue().size() > 0) {
-			Thread.sleep(6000);
-		}
+		WdlApp wdlApp = WdlReader.loadAppFromFile("test-data/long-sleep.yaml");
 
-		WdlApp app = WdlReader.loadAppFromFile("test-data/long-sleep.yaml");
+		Map<String, String> inputs = Map.of("input", "input-file");
 
-		Map<String, String> inputs = new HashMap<String, String>();
-		inputs.put("input", "input-file");
-
-		AbstractJob job1 = createJobFromWdl(app, "job1", inputs);
+		AbstractJob job1 = createJobFromWdl(application, wdlApp, "job1", inputs);
 		engine.submit(job1);
 
-		Thread.sleep(5000);
+		Thread.sleep(500);
 
-		AbstractJob job2 = createJobFromWdl(app, "job2", inputs);
+		AbstractJob job2 = createJobFromWdl(application, wdlApp, "job2", inputs);
 		engine.submit(job2);
 
-		Thread.sleep(5000);
+		Thread.sleep(500);
 
-		AbstractJob job3 = createJobFromWdl(app, "job3", inputs);
+		AbstractJob job3 = createJobFromWdl(application, wdlApp, "job3", inputs);
 		engine.submit(job3);
 
-		Thread.sleep(5000);
+		Thread.sleep(500);
 
-		AbstractJob job4 = createJobFromWdl(app, "job4", inputs);
+		AbstractJob job4 = createJobFromWdl(application, wdlApp, "job4", inputs);
 		engine.submit(job4);
 
-		Thread.sleep(5000);
+		Thread.sleep(500);
 
 		assertEquals(JobState.RUNNING, job1.getState());
 		assertEquals(JobState.WAITING, job2.getState());
@@ -223,9 +208,7 @@ public class PriorityThreadPoolExecutorTest {
 		assertEquals(JobState.CANCELED, job3.getState());
 		assertEquals(JobState.CANCELED, job4.getState());
 
-		while (engine.getAllJobsInLongTimeQueue().size() > 0) {
-			Thread.sleep(6000);
-		}
+		application.stop(null);
 	}
 
 	/**
@@ -236,37 +219,32 @@ public class PriorityThreadPoolExecutorTest {
 	 */
 	@Test
 	public void testMultipleJobsWithPriority() throws Exception {
-
+		TestApplication application = new TestApplication();
 		WorkflowEngine engine = application.getWorkflowEngine();
 
-		while (engine.getAllJobsInLongTimeQueue().size() > 0) {
-			Thread.sleep(6000);
-		}
+		WdlApp wdlApp = WdlReader.loadAppFromFile("test-data/long-sleep.yaml");
 
-		WdlApp app = WdlReader.loadAppFromFile("test-data/long-sleep.yaml");
+		Map<String, String> inputs = Map.of("input", "input-file");
 
-		Map<String, String> inputs = new HashMap<String, String>();
-		inputs.put("input", "input-file");
-
-		AbstractJob job1 = createJobFromWdl(app, "job1_a", inputs);
+		AbstractJob job1 = createJobFromWdl(application, wdlApp, "job1_a", inputs);
 		engine.submit(job1);
 
-		Thread.sleep(5000);
+		Thread.sleep(500);
 
-		AbstractJob job2 = createJobFromWdl(app, "job2_a", inputs);
+		AbstractJob job2 = createJobFromWdl(application, wdlApp, "job2_a", inputs);
 		engine.submit(job2);
 
-		Thread.sleep(5000);
+		Thread.sleep(500);
 
-		AbstractJob job3 = createJobFromWdl(app, "job3_a", inputs);
+		AbstractJob job3 = createJobFromWdl(application, wdlApp, "job3_a", inputs);
 		engine.submit(job3);
 
-		Thread.sleep(5000);
+		Thread.sleep(500);
 
-		AbstractJob job4 = createJobFromWdl(app, "job4_a", inputs);
+		AbstractJob job4 = createJobFromWdl(application, wdlApp, "job4_a", inputs);
 		engine.submit(job4, 0);
 
-		Thread.sleep(5000);
+		Thread.sleep(500);
 
 		assertEquals(JobState.RUNNING, job1.getState());
 		assertEquals(JobState.WAITING, job2.getState());
@@ -327,9 +305,7 @@ public class PriorityThreadPoolExecutorTest {
 		assertEquals(JobState.CANCELED, job3.getState());
 		assertEquals(JobState.CANCELED, job4.getState());
 
-		while (engine.getAllJobsInLongTimeQueue().size() > 0) {
-			Thread.sleep(6000);
-		}
+		application.stop(null);
 	}
 
 	/**
@@ -340,38 +316,33 @@ public class PriorityThreadPoolExecutorTest {
 	 */
 	@Test
 	public void testMultipleJobsAndUpdatePriority() throws Exception {
-
+		TestApplication application = new TestApplication();
 		WorkflowEngine engine = application.getWorkflowEngine();
 
-		while (engine.getAllJobsInLongTimeQueue().size() > 0) {
-			Thread.sleep(6000);
-		}
+		WdlApp wdlApp = WdlReader.loadAppFromFile("test-data/long-sleep.yaml");
 
-		WdlApp app = WdlReader.loadAppFromFile("test-data/long-sleep.yaml");
+		Map<String, String> inputs = Map.of("input", "input-file");
 
-		Map<String, String> inputs = new HashMap<String, String>();
-		inputs.put("input", "input-file");
-
-		AbstractJob job1 = createJobFromWdl(app, "job1_ab", inputs);
+		AbstractJob job1 = createJobFromWdl(application, wdlApp, "job1_ab", inputs);
 		engine.submit(job1);
 
-		Thread.sleep(5000);
+		Thread.sleep(500);
 
-		AbstractJob job2 = createJobFromWdl(app, "job2_ab", inputs);
+		AbstractJob job2 = createJobFromWdl(application, wdlApp, "job2_ab", inputs);
 		engine.submit(job2);
 
-		Thread.sleep(5000);
+		Thread.sleep(500);
 
-		AbstractJob job3 = createJobFromWdl(app, "job3_ab", inputs);
+		AbstractJob job3 = createJobFromWdl(application, wdlApp, "job3_ab", inputs);
 		engine.submit(job3);
 
-		Thread.sleep(5000);
+		Thread.sleep(500);
 
 		// submit with lowest priority
-		AbstractJob job4 = createJobFromWdl(app, "job4_ab", inputs);
+		AbstractJob job4 = createJobFromWdl(application, wdlApp, "job4_ab", inputs);
 		engine.submit(job4);
 
-		Thread.sleep(5000);
+		Thread.sleep(500);
 
 		assertEquals(JobState.RUNNING, job1.getState());
 		assertEquals(JobState.WAITING, job2.getState());
@@ -381,7 +352,8 @@ public class PriorityThreadPoolExecutorTest {
 		assertEquals(JobState.WAITING, job4.getState());
 		assertEquals(2, job4.getPositionInQueue());
 
-		assertTrue(job1.getPriority() < job2.getPriority() && job2.getPriority() < job3.getPriority()
+		assertTrue(job1.getPriority() < job2.getPriority()
+				&& job2.getPriority() < job3.getPriority()
 				&& job3.getPriority() < job4.getPriority());
 
 		assertEquals(4, engine.getAllJobsInLongTimeQueue().size());
@@ -457,13 +429,15 @@ public class PriorityThreadPoolExecutorTest {
 		assertEquals(JobState.CANCELED, job3.getState());
 		assertEquals(JobState.CANCELED, job4.getState());
 
-		while (engine.getAllJobsInLongTimeQueue().size() > 0) {
-			Thread.sleep(6000);
-		}
+		application.stop(null);
 	}
 
-	@Test
-	public CloudgeneJob createJobFromWdl(WdlApp app, String id, Map<String, String> inputs) throws Exception {
+	private CloudgeneJob createJobFromWdl(
+			TestApplication application,
+			WdlApp wdlApp,
+			String id,
+			Map<String, String> inputs) throws Exception {
+
 		UserDao userDao = new UserDao(application.getDatabase());
 		User user = userDao.findByUsername("user");
 
@@ -477,14 +451,14 @@ public class PriorityThreadPoolExecutorTest {
 		workspace.setJob(id);
 		workspace.setup();
 
-		CloudgeneJob job = new CloudgeneJob(user, id, app, inputs);
+		CloudgeneJob job = new CloudgeneJob(user, id, wdlApp, inputs);
 		job.setId(id);
 		job.setName(id);
 		job.setWorkspace(workspace);
 		job.setLocalWorkspace(localWorkspace);
 		job.setSettings(settings);
-		job.setApplication(app.getName() + " " + app.getVersion());
-		job.setApplicationId(app.getId());
+		job.setApplication(wdlApp.getName() + " " + wdlApp.getVersion());
+		job.setApplicationId(wdlApp.getId());
 
 		return job;
 	}
