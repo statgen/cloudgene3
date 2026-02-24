a34325dc8936c0e65cb1956c2e31b210778dadb2
Marc Fraile <marc.fraile.fabrega@gmail.com>
2026-02-24 14:04:04 -0500

    Bugfix: add missing state values in schema.

--- src/main/sql/updates.sql
+++ src/main/sql/updates.sql
@@ -208,6 +208,6 @@ CREATE TABLE banners (
 
 -- 3.1.4-statgen.13
 
-ALTER TABLE job ADD COLUMN completion_state ENUM('submitted', 'running', 'complete', 'retired') NULL DEFAULT NULL;
-ALTER TABLE job ADD COLUMN success_state ENUM('pending', 'succeeded', 'failed', 'canceled') NULL DEFAULT NULL;
+ALTER TABLE job ADD COLUMN completion_state ENUM('submitted', 'running', 'complete', 'retired', 'deleted') NULL DEFAULT NULL;
+ALTER TABLE job ADD COLUMN success_state ENUM('pending', 'succeeded', 'failed', 'canceled', 'dead') NULL DEFAULT NULL;
 ALTER TABLE job ADD COLUMN notification_state ENUM('pending', 'notified_completion', 'sent_retirement_reminder', 'notified_retirement') NULL DEFAULT NULL;
