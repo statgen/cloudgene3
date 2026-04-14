82aeed90ac1efd127fb286ecd06ec994b2d2da59
Marc Fraile <marc.fraile.fabrega@gmail.com>
2026-04-14 16:09:18 -0400

    More try-with-resources improvements.

--- src/main/java/cloudgene/mapred/database/util/JdbcDataAccessObject.java
+++ src/main/java/cloudgene/mapred/database/util/JdbcDataAccessObject.java
@@ -69,8 +69,8 @@ public abstract class JdbcDataAccessObject {
 	}
 
 	public int insert(String sql, Object[] params) throws SQLException {
-		try (Connection connection = database.getDataSource().getConnection()) {
-			PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+		try (Connection connection = database.getDataSource().getConnection();
+			PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
 
 			runner.fillStatement(statement, params);
 
@@ -79,6 +79,7 @@ public abstract class JdbcDataAccessObject {
 			ResultSet rs = statement.getGeneratedKeys();
 			rs.beforeFirst();
 			rs.next();
+
 			return rs.getInt(1);
 		}
 	}
