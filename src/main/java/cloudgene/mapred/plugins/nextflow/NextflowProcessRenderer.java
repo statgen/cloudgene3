6ed2af46b1878ace4bef8db51f15cffe4747d1ad
Marc Fraile <marc.fraile.fabrega@gmail.com>
2026-04-15 14:56:21 -0400

    Make templates/progressbar more accessible; fix some of the mess in NextflowProcessRenderer.

--- src/main/java/cloudgene/mapred/plugins/nextflow/NextflowProcessRenderer.java
+++ src/main/java/cloudgene/mapred/plugins/nextflow/NextflowProcessRenderer.java
@@ -1,109 +1,97 @@
 package cloudgene.mapred.plugins.nextflow;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.Map;
-
+import cloudgene.mapred.jobs.Message;
+import groovy.text.SimpleTemplateEngine;
+import groovy.text.Template;
+import io.micronaut.core.annotation.NonNull;
 import org.codehaus.groovy.control.CompilationFailedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import cloudgene.mapred.jobs.Message;
-import genepi.io.FileUtil;
-import groovy.text.SimpleTemplateEngine;
-import groovy.text.Template;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NextflowProcessRenderer {
 
-	private static final String TEMPLATES_LIST = "/templates/list.html";
-
-	private static final String TEMPLATES_LABEL = "/templates/label.html";
-
-	private static final String TEMPLATES_STATUS = "/templates/status.html";
-
-	private static final String TEMPLATES_PROGRESSBAR = "/templates/progressbar.html";
-
-	private static final String FAILED = "FAILED";
-
-	private static final String KILLED = "KILLED";
-
-	private static final String VIEW_PROGRESSBAR = "progressbar";
-
-	private static final String VIEW_LABEL = "label";
-
-	private static final String VIEW_STATUS = "status";
-
-	private static final String TRACE_STATUS = "status";
-
-	private static final String SUBMITTED = "SUBMITTED";
-
-	private static final String COMPLETED = "COMPLETED";
-
-	private static final String RUNNING = "RUNNING";
-
-	public static final Map<String, Template> CACHE = new HashMap<String, Template>();
-
 	private static final Logger log = LoggerFactory.getLogger(NextflowProcessRenderer.class);
 
-	public static void render(NextflowProcessConfig config, NextflowProcess process, Message message) {
+	private static final Map<String, Template> CACHE = new HashMap<>();
+
+	private static final String TEMPLATES_LIST = "/templates/list.html";
+	private static final String TEMPLATES_LABEL = "/templates/label.html";
+	private static final String TEMPLATES_STATUS = "/templates/status.html";
+	private static final String TEMPLATES_PROGRESSBAR = "/templates/progressbar.html";
+
+	private static final String SUBMITTED = "SUBMITTED";
+	private static final String RUNNING = "RUNNING";
+	private static final String COMPLETED = "COMPLETED";
+	private static final String FAILED = "FAILED";
+	private static final String KILLED = "KILLED";
+
+	private static final String VIEW_LABEL = "label";
+	private static final String VIEW_STATUS = "status";
+	private static final String VIEW_PROGRESSBAR = "progressbar";
+	private static final String TRACE_STATUS = "status";
+
+	public static void render(
+			@NonNull NextflowProcessConfig config,
+			@NonNull NextflowProcess process,
+			@NonNull Message message) {
 
 		String label = (config.getLabel() != null ? config.getLabel() : process.getName());
 
 		switch (config.getView()) {
-		case VIEW_PROGRESSBAR:
-			NextflowProcessRenderer.renderAsProgressbar(label, process, message);
-			break;
-		case VIEW_LABEL:
-			NextflowProcessRenderer.renderAsLabel(label, process, message);
-			break;
-		case VIEW_STATUS:
-			NextflowProcessRenderer.renderAsStatus(label, process, message);
-			break;
-		default:
-			NextflowProcessRenderer.renderAsList(label, process, message);
+			case VIEW_PROGRESSBAR:
+				render(label, TEMPLATES_PROGRESSBAR, process, message);
+				break;
+			case VIEW_LABEL:
+				render(label, TEMPLATES_LABEL, process, message);
+				break;
+			case VIEW_STATUS:
+				render(label, TEMPLATES_STATUS, process, message);
+				break;
+			default:
+				render(label, TEMPLATES_LIST, process, message);
 		}
 	}
 
-	public static void renderAsLabel(String label, NextflowProcess process, Message message) {
-		render(label, TEMPLATES_LABEL, process, message);
-	}
-
-	public static void renderAsList(String label, NextflowProcess process, Message message) {
-		render(label, TEMPLATES_LIST, process, message);
-	}
-
-	public static void renderAsStatus(String label, NextflowProcess process, Message message) {
-		render(label, TEMPLATES_STATUS, process, message);
-	}
-
-	public static void renderAsProgressbar(String label, NextflowProcess process, Message message) {
-		render(label, TEMPLATES_PROGRESSBAR, process, message);
-	}
-
-	public static void render(String label, String template, NextflowProcess process, Message message) {
+	private static void render(
+			@NonNull String label,
+			@NonNull String template,
+			@NonNull NextflowProcess process,
+			@NonNull Message message) {
 
 		int running = 0;
 		int completed = 0;
 		int failed = 0;
-		for (NextflowTask task : process.getTasks()) {
 
+		for (NextflowTask task : process.getTasks()) {
 			String status = (String) task.getTrace().get(TRACE_STATUS);
 
 			if (status.equals(RUNNING) || status.equals(SUBMITTED)) {
 				running++;
 			}
+
 			if (status.equals(COMPLETED)) {
 				completed++;
 			}
+
 			if (status.equals(FAILED) || status.equals(KILLED)) {
 				failed++;
 			}
 		}
 
 		int total = running + completed + failed;
-		Map<String, Object> bindings = new HashMap<String, Object>();
+		String id = label.trim().toLowerCase().replaceAll("[\\s_-]+", "-");
+
+		Map<String, Object> bindings = new HashMap<>();
+		bindings.put("id", id);
 		bindings.put("label", label);
 		bindings.put("total", total);
 		bindings.put("running", running);
@@ -115,9 +103,10 @@ public class NextflowProcessRenderer {
 			String text = renderTemplate(template, bindings);
 			message.setMessage(text);
 		} catch (Exception e) {
-			message.setMessage("Template could not be rendered: " + e.toString());
+			message.setMessage("Template could not be rendered: " + e);
 			log.error("Template could not be rendered ", e);
 		}
+
 		if (running > 0) {
 			message.setType(Message.RUNNING);
 		} else if (completed > 0) {
@@ -125,34 +114,33 @@ public class NextflowProcessRenderer {
 		} else {
 			message.setType(Message.ERROR);
 		}
-
 	}
 
-	public static String renderTemplate(String path, Map<String, Object> bindings)
+	private static @NonNull String renderTemplate(@NonNull String path, @NonNull Map<String, Object> bindings)
 			throws CompilationFailedException, ClassNotFoundException, IOException, URISyntaxException {
+
 		Template template = getTemplate(path);
 		String rendered = template.make(bindings).toString();
-		return rendered.replaceAll("\n", "");
+		return rendered.replace("\n", "");
 	}
 
-	public static synchronized Template getTemplate(String path)
-			throws IOException, URISyntaxException, CompilationFailedException, ClassNotFoundException {
+	private static synchronized @NonNull Template getTemplate(@NonNull String path)
+			throws IOException, CompilationFailedException, ClassNotFoundException, URISyntaxException {
+
 		Template template = CACHE.get(path);
 
 		if (template != null) {
 			return template;
 		}
 
+		URL resourceUrl = NextflowProcessRenderer.class.getResource(path);
+		Path resourcePath = Paths.get(resourceUrl.toURI());
+		String content = Files.readString(resourcePath);
+
 		SimpleTemplateEngine engine = new SimpleTemplateEngine();
-		String content = readTemplate(path);
 		template = engine.createTemplate(content);
 		CACHE.put(path, template);
+
 		return template;
 	}
-
-	private static String readTemplate(String path) {
-		InputStream stream = NextflowProcess.class.getResourceAsStream(path);
-		return FileUtil.readFileAsString(stream);
-	}
-
 }
