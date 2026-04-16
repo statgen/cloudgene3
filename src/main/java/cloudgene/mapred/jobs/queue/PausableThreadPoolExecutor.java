8496a6f25231ce452756075b5a67f742604b394b
Marc Fraile <marc.fraile.fabrega@gmail.com>
2026-04-16 14:45:34 -0400

    PausableThreadPoolExecutor: code quality.

--- src/main/java/cloudgene/mapred/jobs/queue/PausableThreadPoolExecutor.java
+++ src/main/java/cloudgene/mapred/jobs/queue/PausableThreadPoolExecutor.java
@@ -12,26 +12,23 @@ import java.util.concurrent.locks.ReentrantLock;
  * A light wrapper around the {@link ThreadPoolExecutor}. It allows for you to
  * pause execution and resume execution when ready. It is very handy for games
  * that need to pause.
- * 
+ *
  * @author Matthew A. Johnston (warmwaffles)
  */
 public class PausableThreadPoolExecutor extends ThreadPoolExecutor {
 	private boolean isPaused;
-	private ReentrantLock lock;
-	private Condition condition;
+	private final ReentrantLock lock;
+	private final Condition condition;
 
 	/**
-	 * @param corePoolSize
-	 *            The size of the pool
-	 * @param maximumPoolSize
-	 *            The maximum size of the pool
-	 * @param keepAliveTime
-	 *            The amount of time you wish to keep a single task alive
-	 * @param unit
-	 *            The unit of time that the keep alive time represents
-	 * @param workQueue
-	 *            The queue that holds your tasks
-	 * @see {@link ThreadPoolExecutor#ThreadPoolExecutor(int, int, long, TimeUnit, BlockingQueue)}
+	 * @param corePoolSize    The size of the pool.
+	 * @param maximumPoolSize The maximum size of the pool.
+	 * @param keepAliveTime   The amount of time you wish to keep a single task
+	 *                        alive.
+	 * @param unit            The unit of time that the keep alive time represents.
+	 * @param workQueue       The queue that holds your tasks.
+	 * @see ThreadPoolExecutor#ThreadPoolExecutor(int, int, long, TimeUnit,
+	 *      BlockingQueue)
 	 */
 	public PausableThreadPoolExecutor(int corePoolSize, int maximumPoolSize,
 			long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
@@ -41,11 +38,9 @@ public class PausableThreadPoolExecutor extends ThreadPoolExecutor {
 	}
 
 	/**
-	 * @param thread
-	 *            The thread being executed
-	 * @param runnable
-	 *            The runnable task
-	 * @see {@link ThreadPoolExecutor#beforeExecute(Thread, Runnable)}
+	 * @param thread   The thread being executed.
+	 * @param runnable The runnable task.
+	 * @see ThreadPoolExecutor#beforeExecute(Thread, Runnable)
 	 */
 	@Override
 	protected void beforeExecute(Thread thread, Runnable runnable) {
@@ -63,10 +58,6 @@ public class PausableThreadPoolExecutor extends ThreadPoolExecutor {
 
 	public boolean isRunning() {
 		return !isPaused;
-	}
-
-	public boolean isPaused() {
-		return isPaused;
 	}
 
 	/**
@@ -93,40 +84,42 @@ public class PausableThreadPoolExecutor extends ThreadPoolExecutor {
 			lock.unlock();
 		}
 	}
-	
+
 	@Override
 	protected <T> RunnableFuture<T> newTaskFor(Runnable runnable, T value) {
-		return new ComparableFutureTask<T>(runnable, value);
+		return new ComparableFutureTask<>(runnable, value);
 	}
-	
-	protected class ComparableFutureTask<T> 
-    extends FutureTask<T> implements Comparable<ComparableFutureTask<T>> {
 
-        private Object object;
+	private static class ComparableFutureTask<T>
+			extends FutureTask<T> implements Comparable<ComparableFutureTask<T>> {
 
-        public ComparableFutureTask(Runnable runnable, T result) {
-            super(runnable, result);
-            object = runnable;
-        }
+		private final Runnable runnable;
 
-        @Override
-        @SuppressWarnings({ "unchecked", "rawtypes" })
-        public int compareTo(ComparableFutureTask<T> o) {
-            if (this == o) {
-                return 0;
-            }
-            if (o == null) {
-                return -1; // this has higher priority than null
-            }
-            if (object != null && o.object != null) {
-                if (object.getClass().equals(o.object.getClass())) {
-                    if (object instanceof Comparable) {
-                        return ((Comparable) object).compareTo(o.object);
-                    }
-                }
-            }
-            return 0;
-        }
-    }
+		public ComparableFutureTask(Runnable runnable, T result) {
+			super(runnable, result);
+			this.runnable = runnable;
+		}
 
+		@Override
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		public int compareTo(ComparableFutureTask<T> that) {
+			if (this == that) {
+				return 0;
+			}
+
+			if (that == null) {
+				return -1; // this has higher priority than null
+			}
+
+			if (this.runnable != null && that.runnable != null) {
+				if (this.runnable.getClass().equals(that.runnable.getClass())) {
+					if (this.runnable instanceof Comparable) {
+						return ((Comparable) this.runnable).compareTo(that.runnable);
+					}
+				}
+			}
+
+			return 0;
+		}
+	}
 }
