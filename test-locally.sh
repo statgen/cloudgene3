820035e1769cb87fb508c3aacd2740b1aea7e09d
Marc Fraile <marc.fraile.fabrega@gmail.com>
2026-05-11 16:32:31 -0400

    test-locally.sh: use MarcFraile/dummy-cloudgene-app

--- test-locally.sh
+++ test-locally.sh
@@ -9,8 +9,7 @@ mvn clean install -DskipTests
 # Set Cloudgene up
 cd target/cloudgene-*-statgen.*/
 
-./cloudgene install lukfor/cg-fetchngs
-# ./cloudgene install statgen/imputationserver2
+./cloudgene install MarcFraile/dummy-cloudgene-app
 
 # Start Cloudgene as a background process (so we can do more stuff)
 ./cloudgene server &
@@ -18,7 +17,7 @@ cd target/cloudgene-*-statgen.*/
 # Wait for Cloudgene to be up and running, and submit a 'fetchngs' job with user 'foobar'
 sleep 2
 FOOBAR_TOKEN=$(curl -d 'username=foobar' --data-urlencode 'password=foobarBAZ+42!' 'http://localhost:8082/login' | jq -r '.access_token')
-curl -H "X-Auth-Token: ${FOOBAR_TOKEN}" -F 'input=SRR12696236' 'http://localhost:8082/api/v2/jobs/submit/fetchngs'
+curl -H "X-Auth-Token: ${FOOBAR_TOKEN}" -X POST 'http://localhost:8082/api/v2/jobs/submit/dummy-cloudgene-app'
 
 # Set Cloudgene as the foreground process again (so it dies when we kill the script)
 fg %1
